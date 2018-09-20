038ef2d67286f959a844a65a76224b0c57a87cae
David Nolan <davidnln13@yahoo.com>
2018-09-20 20:39:57 +0100

    working on project

--- SFML project 1/SFML Project 1 Sprite Movement/Player.cpp
+++ SFML project 1/SFML Project 1 Sprite Movement/Player.cpp
@@ -2,45 +2,140 @@
 
 Player::Player(float posX, float posY, int rotation)
 {
+	m_position = sf::Vector2f(posX, posY);
+	m_currentVelocity = sf::Vector2f(0,0);
+	m_rotation = rotation;
+	m_linearAccel = 0.02;
+	m_angularAccel = 1;
+
 	if (!m_texture.loadFromFile("pacman.png"))
 	{
 		std::string s("Error loading texture");
 		throw std::exception(s.c_str());
 	}
-	m_sprite.setTexture(m_texture);
 
-	m_sprite.setPosition(posX, posY);
-	m_sprite.setRotation(rotation);
+	m_sprite.setTexture(m_texture);
+	m_sprite.setPosition(m_position.x, m_position.y);
+	m_sprite.setRotation(m_rotation);
 	m_sprite.setOrigin(sf::Vector2f(m_sprite.getTexture()->getSize().x * 0.5, m_sprite.getTexture()->getSize().y * 0.5));
 }
 void Player::Move()
 {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
-		m_sprite.move(0, -0.5);
-		m_sprite.setRotation(270);
+		IncreaseSpeed("UP");
+		checkIfMoving = true;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 	{
-		m_sprite.move(0, +0.5);
-		m_sprite.setRotation(90);
+		IncreaseSpeed("DOWN");
+		checkIfMoving = true;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
-		m_sprite.move(-0.5,0);
-		m_sprite.setRotation(180);
+		IncreaseSpeed("LEFT");
+		checkIfMoving = true;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
-		m_sprite.move(+0.5, 0);
-		m_sprite.setRotation(0);
+		IncreaseSpeed("RIGHT");
+		checkIfMoving = true;
 	}
 }
 void Player::Update()
 {
+	std::cout << "Y velo " << m_currentVelocity.y << " Y accel" << m_linearAccel << std::endl;
 	Move();
+	CheckSpeedBoundaries();
+	if (m_currentVelocity.x == 0 && m_currentVelocity.y == 0)
+	{
+		checkIfMoving = false;
+	}
+	if (checkIfMoving == true)
+	{
+		DecreaseSpeed();
+	}
+	m_sprite.setRotation(m_rotation);
+	m_sprite.move(m_currentVelocity.x, m_currentVelocity.y);
+}
+void Player::IncreaseSpeed(std::string direction)
+{
+	if (direction == "UP" && m_currentVelocity.y > -1)
+	{
+		m_currentVelocity.y -= m_linearAccel;
+	}
+	if (direction == "DOWN" && m_currentVelocity.y < 1)
+	{
+		m_currentVelocity.y += m_linearAccel;
+	}
+	if (direction == "LEFT" && m_currentVelocity.x > -1)
+	{
+		m_currentVelocity.x -= m_linearAccel;
+	}
+	if (direction == "RIGHT" && m_currentVelocity.x < 1)
+	{
+		m_currentVelocity.y += m_linearAccel;
+	}
+}
+void Player::DecreaseSpeed()
+{
+	if (m_currentVelocity.x > 0)
+	{
+		m_currentVelocity.x -= 0.01;
+		if (m_currentVelocity.x < 0.1)
+		{
+			m_currentVelocity.x = 0;
+		}
+	}
+
+	if (m_currentVelocity.x < 0)
+	{
+		m_currentVelocity.x += 0.01;
+		if (m_currentVelocity.x > -0.1)
+		{
+			m_currentVelocity.x = 0;
+		}
+	}
+
+	if (m_currentVelocity.y > 0)
+	{
+		m_currentVelocity.y -= 0.01;
+		if (m_currentVelocity.y < 0.1)
+		{
+			m_currentVelocity.y = 0;
+		}
+	}
+
+	if (m_currentVelocity.y < 0)
+	{
+		m_currentVelocity.y += 0.01;
+		if (m_currentVelocity.y > -0.1)
+		{
+			m_currentVelocity.y = 0;
+		}
+	}
 }
 
+void Player::CheckSpeedBoundaries()
+{
+	if (m_currentVelocity.y < -1)
+	{
+		m_currentVelocity.y = -1;
+	}
+	if (m_currentVelocity.y > 1)
+	{
+		m_currentVelocity.y = 1;
+	}
+	if (m_currentVelocity.x < -1)
+	{
+		m_currentVelocity.x = -1;
+	}
+	if (m_currentVelocity.x > 1)
+	{
+		m_currentVelocity.x = 1;
+	}
+	
+}
 void Player::Render(sf::RenderWindow &window)
 {
 	window.draw(m_sprite);
@@ -50,3 +145,4 @@ sf::Sprite& Player::getSprite()
 {
 	return m_sprite;
 }
+
--- SFML project 1/SFML Project 1 Sprite Movement/Player.h
+++ SFML project 1/SFML Project 1 Sprite Movement/Player.h
@@ -10,8 +10,16 @@ public:
 	void Update();
 	void Render(sf::RenderWindow& window);
 	sf::Sprite& getSprite();
-
+	void IncreaseSpeed(std::string direction);
+	void DecreaseSpeed();
+	void CheckSpeedBoundaries();
 private:
 	sf::Sprite m_sprite;
+	bool checkIfMoving = false;
 	sf::Texture m_texture;
+	sf::Vector2f m_position;
+	sf::Vector2f m_currentVelocity;
+	float m_rotation;
+	float m_linearAccel;
+	float m_angularAccel;
 };
